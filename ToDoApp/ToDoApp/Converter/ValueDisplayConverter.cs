bb79a0f2c6fec19d86d0026441be8c3c653b3b5e
Sammo <sammo.wong@outlook.com>
2020-05-11 23:44:08 +0800

    update

---
+++ ToDoApp/ToDoApp/Converter/ValueDisplayConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ToDoApp.Converter
+{
+    public class ValueDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null && int.TryParse(value.ToString(), out int result))
+            {
+                if (result == 0)
+                    return string.Empty;
+                return result;
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using ToDoApp.Model;
+
+namespace ToDoApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            menuModels = new ObservableCollection<MenuModel>();
+            menuModels.Add(new MenuModel { IconFont = "\xe635", Title = "我的一天", BackColor = "#218868" });
+            menuModels.Add(new MenuModel { IconFont = "\xe6b6", Title = "重要", BackColor = "#EE3B3B" });
+            menuModels.Add(new MenuModel { IconFont = "\xe6e1", Title = "已计划日程", BackColor = "#218868" });
+            menuModels.Add(new MenuModel { IconFont = "\xe614", Title = "已分配给我", BackColor = "#EE3B3B" });
+            menuModels.Add(new MenuModel { IconFont = "\xe755", Title = "任务", BackColor = "#218868" });
+
+            menuSubModels = new ObservableCollection<MenuModel>();
+            menuSubModels.Add(new MenuModel { Title = "购物清单" });
+            menuSubModels.Add(new MenuModel { Title = "杂货清单" });
+            menuSubModels.Add(new MenuModel { Title = "待办事项" });
+        }
+
+        private ObservableCollection<MenuModel> menuModels;
+
+        public ObservableCollection<MenuModel> MenuModels
+        {
+            get { return menuModels; }
+            set { menuModels = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<MenuModel> menuSubModels;
+
+        public ObservableCollection<MenuModel> MenuSubModels
+        {
+            get { return menuSubModels; }
+            set { menuSubModels = value; RaisePropertyChanged(); }
+        }
+    }
+}
