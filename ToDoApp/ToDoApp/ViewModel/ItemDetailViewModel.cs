3bc5fd7a5b4cd9db07a7fc23778cb49cb6977f85
Sammo <sammo.wong@outlook.com>
2020-05-13 16:54:45 +0800

    update

--- ToDoApp/ToDoApp/ViewModel/ItemDetailViewModel.cs
+++ ToDoApp/ToDoApp/ViewModel/ItemDetailViewModel.cs
@@ -18,7 +18,7 @@ namespace ToDoApp.ViewModel
             TaskInfos.Add(new TaskInfo { Content = "ObservableCollection" });
             TaskInfos.Add(new TaskInfo { Content = "ObservableCollection" });
             TaskInfos.Add(new TaskInfo { Content = "ObservableCollection" });
-            
+
 
             ExcludeCommand = new RelayCommand<TaskInfo>(arg =>
             {
@@ -32,6 +32,9 @@ namespace ToDoApp.ViewModel
             {
                 arg.IsFavorite = !arg.IsFavorite;
             });
+
+            AddCommand = new RelayCommand(AddTask);
+            DeleteCommand = new RelayCommand<TaskInfo>(t => DeleteTask(t));
         }
 
         private ObservableCollection<TaskInfo> taskInfos = new ObservableCollection<TaskInfo>();
@@ -42,7 +45,33 @@ namespace ToDoApp.ViewModel
             set { taskInfos = value; RaisePropertyChanged(); }
         }
 
+        private string content = string.Empty;
+        public string Content
+        {
+            get { return content; }
+            set { content = value; RaisePropertyChanged(); }
+        }
+
         public RelayCommand<TaskInfo> ExcludeCommand { get; set; }
         public RelayCommand<TaskInfo> FavoriteCommand { get; set; }
+
+        //新增
+        public RelayCommand AddCommand { get; set; }
+        //删除
+        public RelayCommand<TaskInfo> DeleteCommand { get; set; }
+
+        public void AddTask()
+        {
+            if (string.IsNullOrWhiteSpace(Content))
+                return;
+
+            TaskInfos.Add(new TaskInfo { Content = Content });
+            Content = string.Empty;
+        }
+
+        public void DeleteTask(TaskInfo taskInfo)
+        {
+            taskInfos.Remove(taskInfo);
+        }
     }
 }
